716abe3efb081f0769d5bf98c43308db6a3f065b
sirGoulis14 <t.goulianos@gmail.com>
2025-01-12 18:53:51 +0200

    modeling

--- eBallot/settings.py
+++ eBallot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "voting",
 ]
 
 MIDDLEWARE = [
@@ -99,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
+AUTH_USER_MODEL = 'voting.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
---
+++ voting/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.conf import settings
+
+
+class CustomUser(AbstractUser):
+    role = models.CharField(
+        max_length=20,
+        choices=[('Admin', 'Admin'), ('Voter', 'Voter')],
+        default='Voter'
+    )
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',  # Προσαρμοσμένο related_name για αποφυγή σύγκρουσης
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_set',  # Προσαρμοσμένο related_name για αποφυγή σύγκρουσης
+        blank=True
+    )
+
+
+class Election(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    is_active = models.BooleanField(default=False)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=255)
+    election = models.ForeignKey(
+        Election,
+        on_delete=models.CASCADE,
+        related_name='candidates'
+    )
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
